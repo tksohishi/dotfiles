e453d443eec4eb1a6fbe434f6022a8a74ff42cf3
Takeshi Ohishi <tks.ohishi@gmail.com>
2013-01-10 11:35:16 -0800

    Fix npm binary path

--- .zsh_custom/zsh_custom.zsh
+++ .zsh_custom/zsh_custom.zsh
@@ -141,12 +141,12 @@ bindkey "^n" history-beginning-search-forward-end
 bindkey "\\en" history-beginning-search-forward-end
 # }}}
 
-# path {{{
+# PATH {{{
 # local binary
 [ -d $HOME/local/bin ] && export PATH=$HOME/local/bin:$PATH
 
 # node.js and npm
-[ -f /usr/local/share/npm/bin ] && export PATH=$PATH:/usr/local/share/npm/bin
+[ -d /usr/local/share/npm/bin ] && export PATH=$PATH:/usr/local/share/npm/bin
 [ -d /usr/local/lib/node ] && export NODE_PATH=/usr/local/lib/node_modules
 # }}}
 
