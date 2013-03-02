902fd79ff3b9048df503692a86cac45d6a8f0736
Takeshi Ohishi <tks.ohishi@gmail.com>
2013-03-01 19:09:00 -0800

    Remove plugins.zsh

--- .zsh_custom/plugins.zsh
+++
@@ -1,2 +0,0 @@
-# looks like it's not loaded. just keeping to put them in .zshrc
-#plugins=(ant autojump bundler cpanm django gem git github heroku mercurial pip python rails rails3 redis-cli ruby rvm ssh-agent screen sublime svn vundle)
